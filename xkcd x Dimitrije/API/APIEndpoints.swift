701f177ed092bfe8a449cb1b50e59a4e4ab4a225
Dimitrije Pesic <dimitrije.pesic@protonmail.com>
2023-04-24 01:25:00 +0200

    Endpoints for Current/Specific Comic

--- xkcd x Dimitrije/API/APIEndpoints.swift
+++ xkcd x Dimitrije/API/APIEndpoints.swift
@@ -3,7 +3,25 @@
 //  xkcd x Dimitrije
 //
 //  Created by Dimitrije Pesic on 24/04/2023.
-//  Possible enum here for handling different API endpoints.
+//  For MVP - specific or current comic API url calculation
 //
 
 import Foundation
+
+enum APIEndpoints {
+    private static let baseUrl = "https://xkcd.com/"
+    private static let jsonPath = "/info.0.json"
+    
+    case currentComic
+    case comic(Int)
+    
+    var url: URL? {
+        switch self {
+        case .currentComic:
+            return URL(string: APIEndpoints.baseUrl + APIEndpoints.jsonPath)
+        case .comic(let number):
+            return URL(string: APIEndpoints.baseUrl + "\(number)" + APIEndpoints.jsonPath)
+        }
+    }
+    
+}
