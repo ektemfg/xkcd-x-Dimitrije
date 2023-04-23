3db3402bc77046881c832ca2aaa7599596a2f89b
Dimitrije Pesic <dimitrije.pesic@protonmail.com>
2023-04-24 01:37:52 +0200

    Simple yet awesome logger

--- xkcd x Dimitrije/Utils/Logger.swift
+++ xkcd x Dimitrije/Utils/Logger.swift
@@ -3,7 +3,40 @@
 //  xkcd x Dimitrije
 //
 //  Created by Dimitrije Pesic on 24/04/2023.
-//  Love having custom logger, especially for throubleshooting / debug. Will be like a print function on steroids.
+//  Lets finish a Logger so we can implement it while we write functions
 //
 
 import Foundation
+
+class Logger {
+    
+    enum LogReason {
+        case info
+        case warning
+        case error
+        case armageddon
+    }
+    
+    static func log(_ text: String, reason: LogReason = .info) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss.SS"
+        let timestamp = dateFormatter.string(from: Date())
+        let message = "\(reason.symbol) \(timestamp) – \(text)"
+        print(message)
+    }
+}
+
+extension Logger.LogReason {
+    var symbol: Character {
+        switch self {
+        case .info:
+            return "🤔"
+        case .warning:
+            return "😳"
+        case .error:
+            return "😵"
+        case .armageddon:
+            return "🤯"
+        }
+    }
+}
