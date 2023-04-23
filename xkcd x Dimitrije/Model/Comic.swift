360689f6a7bfd10d6a0bcfd29b012f5c1c1db5ea
Dimitrije Pesic <dimitrije.pesic@protonmail.com>
2023-04-24 00:50:09 +0200

    Comic Model + CodingKeys

--- xkcd x Dimitrije/Model/Comic.swift
+++ xkcd x Dimitrije/Model/Comic.swift
@@ -3,7 +3,26 @@
 //  xkcd x Dimitrije
 //
 //  Created by Dimitrije Pesic on 24/04/2023.
-//  Comic Object + CodingKeys will be here.
 //
 
 import Foundation
+
+struct Comic: Codable {
+    let month: String
+    let num: Int
+    let link, year, news, safeTitle: String
+    let transcript, alt: String
+    let img: String
+    let title, day: String
+    
+    var url : URL? {
+        return URL(string: self.img)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case month, num, link, year, news
+        case safeTitle = "safe_title"
+        case transcript, alt, img, title, day
+    }
+    
+}
