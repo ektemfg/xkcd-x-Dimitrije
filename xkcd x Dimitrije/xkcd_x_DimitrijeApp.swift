0970670914b705873903fdc0e22a910e771b4a55
Dimitrije Pesic <dimitrije.pesic@protonmail.com>
2023-04-24 00:19:33 +0200

    Initial Commit

---
+++ xkcd x Dimitrije/xkcd_x_DimitrijeApp.swift
@@ -0,0 +1,17 @@
+//
+//  xkcd_x_DimitrijeApp.swift
+//  xkcd x Dimitrije
+//
+//  Created by Dimitrije Pesic on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct xkcd_x_DimitrijeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
