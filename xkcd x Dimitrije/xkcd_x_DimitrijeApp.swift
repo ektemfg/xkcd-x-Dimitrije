8d21e455a19608e7c28280a6fbee31e3e4116b38
Dimitrije Pesic <dimitrije.pesic@protonmail.com>
2023-04-24 03:04:13 +0200

    Simple TabView Menu

--- xkcd x Dimitrije/xkcd_x_DimitrijeApp.swift
+++ xkcd x Dimitrije/xkcd_x_DimitrijeApp.swift
@@ -6,12 +6,54 @@
 //
 
 import SwiftUI
+import UIKit
 
 @main
 struct xkcd_x_DimitrijeApp: App {
+    @State private var selectedTab = 1
+    init() {
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        UITabBar.appearance().isTranslucent = true
+        UITabBar.appearance().backgroundColor = .white
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZStack(alignment: .bottom) {
+                TabView(selection: $selectedTab) {
+                    NavigationView {
+                        HomeView()
+                    }
+                    .tabItem {
+                        Label("Search", systemImage: "magnifyingglass")
+                    }
+                    .tag(0)
+                    
+                    NavigationView {
+                        HomeView()
+                    }
+                    .tabItem {
+                        Image(systemName: "arrow.triangle.2.circlepath")
+                    }
+                    .tag(1)
+                    
+                    NavigationView {
+                        HomeView()
+                    }
+                    .tabItem {
+                        Label("Favourites", systemImage: selectedTab == 2 ? "heart.fill" : "heart")
+                    }
+                    .tag(2)
+                }
+                .edgesIgnoringSafeArea(.bottom)
+                
+                Divider()
+                    .background(Color.gray)
+                    .frame(height: 1)
+                    .offset(y: -49)
+                    .padding(.bottom, 1)
+                
+            }
         }
     }
-}
+        }
